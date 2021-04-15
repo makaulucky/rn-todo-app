022035ceb085067be095c52af9b5c3e7796e6557
makaulucky <makaulucky5@gmail.com>
2021-04-15 18:30:46 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   const [outputText, setOutputText] = useState ('Click the Button!')
   return (
-    <View style={styles.container}>
-      <Text>Hi</Text>
-      <Text>{outputText}</Text>
-      <Button title="Home" onPress= {() => setOutputText ("The Button Changed Text!")}/>
-      <StatusBar style="auto" />
+    <View style={styles.screen} >
+      <View style={styles.inputcontainer} >
+        <TextInput  placeholder="To do" style={styles.textinput} />
+        <Button title="Add" />
+      </View>
     </View>
   );
-}
+  }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+  
+  const styles = StyleSheet.create({
+    screen: {
+      padding:55
+    },
+    inputcontainer:{
+      flexDirection:'row', 
+      justifyContent:'space-between', 
+      alignItems:'center'
+    },
+    textinput:{
+      width:"85%", 
+        borderColor:'black', 
+        borderWidth:1, 
+        padding:5
+    },
+  });
