edae15430467fd23ee1191edf1018df92b4e000c
makaulucky <makaulucky5@gmail.com>
2021-04-15 13:18:48 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  const [outputText, setOutputText] = useState ('Click the Button!')
+  return (
+    <View style={styles.container}>
+      <Text>Hi</Text>
+      <Text>{outputText}</Text>
+      <Button title="Click Once" onPress= {() => setOutputText ("The Button Changed Text!")} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
